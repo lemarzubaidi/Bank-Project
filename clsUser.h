7d09a9851af0519c015b741ddaf5e61cecae7ece
Lemar Zubaidi <lemarzubaide9@gmail.com>
2026-03-30 18:13:57 +0300

    Update clsUser.h

--- clsUser.h
+++ clsUser.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include "ClsString.h"
 #include "ClsDate.h"
+#include "ClsString.h"
 using namespace std;
 class clsUser:public ClsPerson
 {
@@ -16,7 +17,18 @@ private:
 	int _Premissions;
 
 	bool _MarkedForDelete = false;
+	struct stLoginRegisterRecord;
+	static stLoginRegisterRecord _ConvertLoginRegisterLineToRecord(string Line,string Seperator="#//#")
+	{
+		stLoginRegisterRecord LoginRegisterRecord;
+		vector <string> LoginRegisterDataLine = clsString::Split(Line, Seperator);
+		LoginRegisterRecord.DateTime = LoginRegisterDataLine[0];
+		LoginRegisterRecord.UserName = LoginRegisterDataLine[1];
+		LoginRegisterRecord.Password = LoginRegisterDataLine[2];
+		LoginRegisterRecord.Permissions = stoi(LoginRegisterDataLine[3]);
+		return LoginRegisterRecord;
 
+	}
 	string _PrepareLogInRecord(string Seperator ="#//#")
 	{
 		string LoginRecord = "";
@@ -124,6 +136,14 @@ public:
 	enum enPermissions {
 		eAll=-1,pListClient=1,pAddNewClient=2,pDeleteClient=4,
 		pUpdateClient=8,pFindClient=16,pTranactions=32,pManageUsers=64
+	};
+
+	struct stLoginRegisterRecord
+	{
+		string UserName;
+		string DateTime;
+		string Password;
+		int Permissions;
 	};
 	clsUser(enMode Mode, string FirstName, string LastName,
 		string Email, string Phone, string UserName, string Password,
@@ -314,5 +334,25 @@ public:
 		 }
 
 	 }
-};
 
+	 static vector<stLoginRegisterRecord> GetLoginRegisterList()
+	 {
+		 vector <stLoginRegisterRecord> vLoginRegisterRecord;
+		 fstream MyFile;
+		 MyFile.open("LoginRegister.txt", ios::in);
+		 if (MyFile.is_open())
+		 {
+			 string Line;
+			 stLoginRegisterRecord LoginRegisterRecord;
+			 while (getline(MyFile, Line))
+			 {
+				 LoginRegisterRecord = _ConvertLoginRegisterLineToRecord(Line);
+				 vLoginRegisterRecord.push_back(LoginRegisterRecord);
+
+			 }
+			 MyFile.close();
+
+		 }
+		 return vLoginRegisterRecord;
+	 }
+};
